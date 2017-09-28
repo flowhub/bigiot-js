dad02f7a08dc2627bebb9a10b73222aeaf78f1af
Henri Bergius <henri.bergius@iki.fi>
2017-09-28 16:20:28 +0200

    Implement access token exchange

--- spec/provider.js
+++ spec/provider.js
@@ -2,13 +2,15 @@ const { expect } = require('chai');
 const { provider: BigIotProvider, offering: BigIotOffering } = require('../index');
 
 describe('BIG IoT Provider', () => {
+  const providerId = process.env.BIGIOT_PROVIDER_ID
+  const providerSecret = process.env.BIGIOT_PROVIDER_SECRET
   it('should be possible to instantiate', () => {
     let prov = new BigIotProvider();
     expect(prov).to.be.an('object');
   });
   describe('registering an offering without secrets', () => {
     it('should fail', (done) => {
-      let prov = new BigIotProvider('foo');
+      let prov = new BigIotProvider(providerId);
       let off = new BigIotOffering('test', 'test offering', 'http://example.org/test');
       off.endpoints = {
         uri: 'http://example.net/foo',
@@ -18,16 +20,52 @@ describe('BIG IoT Provider', () => {
       off.extent = {
         city: 'Berlin'
       };
-      prov.register(off)
-      .then(() => {
-        done(new Error('Unauthorized register passed'));
-      })
-      .catch((e) => {
-        expect(e).to.be.an('error');
-        expect(e.message).to.include('NotAuthorized');
-        done();
-      });
+      prov.authenticate()
+        .then(() => {
+          return prov.register(off)
+        })
+        .then((result) => {
+          done(new Error('Unauthorized register passed'));
+        })
+        .catch((e) => {
+          expect(e).to.be.an('error');
+          expect(e.message).to.include('Unauthorized');
+          done();
+        });
       return;
+    });
+  });
+  describe('registering an offering with a secret', () => {
+    it('should succeed', (done) => {
+      let prov = new BigIotProvider(providerId, providerSecret);
+      let off = new BigIotOffering('SpreeTest', 'test offering', 'bigiot:weather');
+      off.endpoints = {
+        uri: 'http://localhost/foo',
+        endpointType: 'HTTP_GET',
+        accessInterfaceType: 'BIGIOT_LIB',
+      };
+      off.outputData = [
+        {
+          name: "temperature",
+          rdfUri: 'http://schema.org/airTemperatureValue',
+        }
+      ];
+      off.extent = {
+        city: 'Berlin'
+      };
+      prov.authenticate()
+        .then(() => {
+          return prov.register(off)
+        })
+        .then((result) => {
+        })
+        .catch((e) => {
+          console.trace(e);
+          done(e);
+        });
+    });
+    it('should have made the offering discoverable', (done) => {
+      this.skip();
     });
   });
 });
