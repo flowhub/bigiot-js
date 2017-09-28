f539703217c8841e8e74334bc733dc5e9b0acb30
Henri Bergius <henri.bergius@iki.fi>
2017-09-28 16:48:57 +0200

    Provide a user-friendly error if trying to register before authenticating

--- spec/provider.js
+++ spec/provider.js
@@ -9,7 +9,7 @@ describe('BIG IoT Provider', () => {
     expect(prov).to.be.an('object');
   });
   describe('registering an offering without secrets', () => {
-    it('should fail', (done) => {
+    it('should fail on authenticate', (done) => {
       let prov = new BigIotProvider(providerId);
       let off = new BigIotOffering('test offering', 'http://example.org/test');
       off.endpoints = {
@@ -21,9 +21,6 @@ describe('BIG IoT Provider', () => {
         city: 'Berlin'
       };
       prov.authenticate()
-        .then(() => {
-          return prov.register(off)
-        })
         .then((result) => {
           done(new Error('Unauthorized register passed'));
         })
@@ -32,6 +29,28 @@ describe('BIG IoT Provider', () => {
           expect(e.message).to.include('Unauthorized');
           done();
         });
+      return;
+    });
+    it('should fail if register is called before authentication', (done) => {
+      let prov = new BigIotProvider(providerId);
+      let off = new BigIotOffering('test offering', 'http://example.org/test');
+      off.endpoints = {
+        uri: 'http://example.net/foo',
+        endpointType: 'HTTP_GET',
+        accessInterfaceType: 'BIGIOT_LIB',
+      };
+      off.extent = {
+        city: 'Berlin'
+      };
+      prov.register(off)
+      .then((result) => {
+        done(new Error('Unauthorized register passed'));
+      })
+      .catch((e) => {
+        expect(e).to.be.an('error');
+        expect(e.message).to.include('must be authenticated');
+        done();
+      });
       return;
     });
   });
